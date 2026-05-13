78ff36ca031d8445d9848b4be3a347139b6204f4
jpv <jesus.pardo.valencia@alumnos.upm.es>
2026-05-13 13:16:22 +0200

    Refactor non-oscillatory_periodicity_spectrum function
    Refactor non-oscillatory periodicity spectrum function to improve clarity and maintainability. Update input argument handling and comments for better understanding.

--- matlab/non_oscillatory_periodicity_spectrum.m
+++ matlab/non_oscillatory_periodicity_spectrum.m
@@ -32,9 +32,7 @@ function results = non_oscillatory_periodicity_spectrum(signal_duration, Fs, win
 % Authors: Jesús Pardo-Valencia, Guglielmo Foffani
 % Version: 2.0.0 
 % Date: March 2026
-% Reference: Pardo-Valencia et al. (2026). The Bayesian periodicity
-% spectrum to quantify oscillatory activity in biological signals. Science
-% Advances.
+% Reference: 
 % Repository: GitHub
 % License: MIT License
 % Copyright (c) 2025 jpv
